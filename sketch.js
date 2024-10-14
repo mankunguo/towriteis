27606ac99875d748aa2d04d035d3ed91a0d20bd2
mankunguo <mankunguo@gmail.com>
2024-10-13 19:21:17 -0700

    1

---
+++ sketch.js
@@ -0,0 +1,71 @@
+function toggleOverlay() {
+  var overlay = document.getElementById("overlay");
+  if (overlay.style.display === "none" || overlay.style.display === "") {
+    overlay.style.display = "flex";
+  } else {
+    overlay.style.display = "none";
+  }
+
+}
+
+function toggleOverlay2() {
+  var overlay2 = document.getElementById("overlay2");
+  if (overlay2.style.display === "none" || overlay2.style.display === "") {
+    overlay2.style.display = "flex";
+  } else {
+    overlay2.style.display = "none";
+  }
+
+}
+
+function toggleOverlay3() {
+  var overlay3 = document.getElementById("overlay3");
+  if (overlay3.style.display === "none" || overlay3.style.display === "") {
+    overlay3.style.display = "flex";
+  } else {
+    overlay3.style.display = "none";
+  }
+}
+
+//-------------------
+
+function toggleOverlay4() {
+  var overlay4 = document.getElementById("overlay4");
+  if (overlay4.style.display === "none" || overlay4.style.display === "") {
+    overlay4.style.display = "flex";
+  } else {
+    overlay4.style.display = "none";
+  }
+
+}
+
+function toggleOverlay5() {
+  var overlay5 = document.getElementById("overlay5");
+  if (overlay5.style.display === "none" || overlay5.style.display === "") {
+    overlay5.style.display = "flex";
+  } else {
+    overlay5.style.display = "none";
+  }
+
+}
+
+function toggleOverlay6() {
+  var overlay6 = document.getElementById("overlay6");
+  if (overlay6.style.display === "none" || overlay6.style.display === "") {
+    overlay6.style.display = "flex";
+  } else {
+    overlay6.style.display = "none";
+  }
+}
+
+function toggleOverlayinfo() {
+  var overlayinfo = document.getElementById("overlayinfo");
+  if (overlayinfo.style.display === "none" || overlayinfo.style.display === "") {
+    overlayinfo.style.display = "flex";
+  } else {
+    overlayinfo.style.display = "none";
+  }
+}
+
+
+
